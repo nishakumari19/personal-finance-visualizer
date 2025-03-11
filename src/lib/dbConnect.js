1165f63d749ddba4c3efb8db1b8c947fb9c70257
Nisha Kumari <kumarinish007@gmail.com>
2025-03-11 21:26:34 +0530

    Updated files

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,28 +1,35 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
 if (!MONGO_URI) {
-  throw new Error("MONGO_URI is missing in environment variables!");
+  throw new Error("MONGODB_URI is missing in environment variables!");
 }
 
-let cached = global.mongoose; 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+
+let cached = global.mongoose || { conn: null, promise: null };
 
 const dbConnect = async () => {
   if (cached.conn) return cached.conn; 
-
   if (!cached.promise) {
     console.log("Connecting to MongoDB...");
-    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
-      console.log("MongoDB Connected!");
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => {
+        console.log("MongoDB Connected!");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("MongoDB Connection Error:", err);
+        throw err;
+      });
   }
 
   cached.conn = await cached.promise;
+  global.mongoose = cached; 
   return cached.conn;
 };
 
