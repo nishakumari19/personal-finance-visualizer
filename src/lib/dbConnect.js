7d13079bc498affc332c95e48ff58c98d5af0e23
Nisha Kumari <kumarinish007@gmail.com>
2025-03-11 20:54:51 +0530

    Updated files

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
 };
 
 export default dbConnect;
